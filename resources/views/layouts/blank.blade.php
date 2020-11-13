24984301d85794afa39df4e4f05056f6a2a19483
Alan <alan.sandoval4147@alumnos.udg.mx>
2020-11-12 21:28:27 -0600

    Tema en el Index
    Se implemento la plantilla al indez ademas de agregarle las opciones que se espera implementar posteriormente

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Blank</title>
+  <title>El Come Sano</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,124 +1,104 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Come Sano</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<style>
-        #wrapper {
-            overflow-x: hidden;
-        }
+@extends('layouts.blank')
 
-        #sidebar-wrapper {
-        min-height: 100vh;
-        margin-left: -15rem;
-        -webkit-transition: margin .25s ease-out;
-        -moz-transition: margin .25s ease-out;
-        -o-transition: margin .25s ease-out;
-        transition: margin .25s ease-out;
-        }
+<style>
 
-        #sidebar-wrapper .sidebar-heading {
-        padding: 0.875rem 1.25rem;
-        font-size: 1.2rem;
-        }
+  .weekdays {
+    margin: 0;
+    padding: 10px 0;
+    background-color:#ddd;
+  }
 
-        #sidebar-wrapper .list-group {
-        width: 15rem;
-        }
+  .weekdays th {
+    width: 13.6%;
+    color: #666;
+    text-align: center;
+  }
 
-        #page-content-wrapper {
-        min-width: 100vw;
-        }
+  .days {
+    padding: 10px 0;
+    background: #eee;
+    margin: 0;
+  }
 
-        #wrapper.toggled #sidebar-wrapper {
-        margin-left: 0;
-        }
-
-        @media (min-width: 768px) {
-        #sidebar-wrapper {
-            margin-left: 0;
-        }
-
-        #page-content-wrapper {
-            min-width: 0;
-            width: 100%;
-        }
-
-        #wrapper.toggled #sidebar-wrapper {
-            margin-left: -15rem;
-        }
-        }
-    </style>
-	
-  </head>
-  <body>
-
-    <div class="d-flex" id="wrapper">
-
-        <!-- Barrao lateral -->
-        <div class="bg-light border-right" id="sidebar-wrapper">
-          <div class="sidebar-heading"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRTUpOLydVG0reyHe_sS2E-9m9_2tbxZxousQ&usqp=CAU" alt="logo" srcset="1x"></div>
-          <div class="list-group list-group-flush">
-            <a href="#" class="list-group-item list-group-item-action bg-light">Edad</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Peso</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">IMC</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Calorias</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
-          </div>
-        </div>
-        <!-- Barra lateral terminada -->
+  .days td {
+    list-style-type: none;
+    width: 13.6%;
+    text-align: center;
+    margin-bottom: 5px;
+    font-size:12px;
+    color: #777;
+  }
     
-        <!-- Contenido de la pagina -->
-        <div id="page-content-wrapper">
+</style>
+
+@section('contenido')
+
+  <h1 class="mt-4">Dieta Actual</h1>
+
+  <table style="width:100%">
+    <tr class="weekdays">
+      <th>Lunes</th>
+      <th>Martes</th>
+      <th>Miercoles</th>
+      <th>Jueves</th>
+      <th>Viernes</th>
+      <th>Sabado</th>
+      <th>Domingo</th>
+    </tr>
+    <tr class="days">
+      <td>Cereal</td> 
+      <td>Cereal</td> 
+      <td>Cereal</td>  
+      <td>Cereal</td> 
+      <td>Cereal</td> 
+      <td>Cereal</td> 
+      <td>Cereal</td> 
+    </tr>
+    <tr class="days">
+      <td>Hamburguesa</td> 
+      <td>Tacos</td> 
+      <td>Pollo</td> 
+      <td>Carne Asada</td> 
+      <td>Frijoles</td> 
+      <td>Pizza</td> 
+      <td>Pozole</td>
+    </tr>
+    <tr class="days">
+      <td>Fruta</td> 
+      <td>Fruta</td>
+      <td>Fruta</td>
+      <td>Fruta</td>
+      <td>Fruta</td>
+      <td>Fruta</td> 
+      <td>Fruta</td>
+    </tr>
+  </table>
     
-          <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button class="btn btn-primary" id="menu-toggle">Abrir datos</button>
-    
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Mi Perfil
-                  </a>
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Configuración</a>
-                    <a class="dropdown-item" href="#"></a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Cerrar sesion</a>
-                  </div>
-                </li>
-              </ul>
+  <h1 class="mt-4">Generacion de dietas</h1>
+  <div style="text-align:center;">
+    <button class="btn btn-primary">Generar Nueva Dieta Basada En Mis Datos</button>
+  </div>
+
+  <h1 class="mt-4">Top platillos nuevos</h1>
+  
+  <div class="row">
+
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">2000 KCal</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">Hamburguesa</div>
+                    </div>
+                </div>
             </div>
-          </nav>
-    
-          <div class="container-fluid">
-            <h1 class="mt-4">Generacion de dietas</h1>
-            <h1 class="mt-4">Top platillos nuevos</h1>
-            <div class="container-sm">
-                <p>Aqui van las fotos</p>
-            </div>
-          </div>
         </div>
-        <!-- Se termina contenido de la pagina -->
-    
-      </div>
+    </div>
+      
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script>
-        $("#menu-toggle").click(function(e) {
-          e.preventDefault();
-          $("#wrapper").toggleClass("toggled");
-        });
-    </script>
-    </body>
-</html>
-<!-- https://startbootstrap.com/templates/simple-sidebar/ -->
+  </div>
+
+<h1 class="mt-4">Ayuda Profesional</h1>
+
+@endsection
