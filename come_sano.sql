63511b1f80da29b31e3fbfdd56910ea0b72465ea
Alan <alan.sandoval4147@alumnos.udg.mx>
2020-12-04 14:28:28 -0600

    Base de datos con 10 Platillos

--- come_sano.sql
+++ come_sano.sql
@@ -14,18 +14,15 @@
 -- Volcando estructura para tabla come_sano.consejos
 CREATE TABLE IF NOT EXISTS `consejos` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `plat_id` bigint(20) unsigned NOT NULL,
+  `platillo_id` bigint(20) unsigned NOT NULL,
   `cons_contenido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `consejos_plat_id_foreign` (`plat_id`),
-  CONSTRAINT `consejos_plat_id_foreign` FOREIGN KEY (`plat_id`) REFERENCES `platillos` (`id`)
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.consejos: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `consejos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `consejos` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.dietas
 CREATE TABLE IF NOT EXISTS `dietas` (
@@ -39,9 +36,22 @@ CREATE TABLE IF NOT EXISTS `dietas` (
   CONSTRAINT `dietas_us_id_foreign` FOREIGN KEY (`us_id`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.dietas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `dietas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dietas` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla come_sano.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.historiales
 CREATE TABLE IF NOT EXISTS `historiales` (
@@ -62,9 +72,7 @@ CREATE TABLE IF NOT EXISTS `historiales` (
   CONSTRAINT `historiales_us_id_foreign` FOREIGN KEY (`us_id`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.historiales: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `historiales` DISABLE KEYS */;
-/*!40000 ALTER TABLE `historiales` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.ingredientes
 CREATE TABLE IF NOT EXISTS `ingredientes` (
@@ -80,14 +88,21 @@ CREATE TABLE IF NOT EXISTS `ingredientes` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.ingredientes: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `ingredientes` DISABLE KEYS */;
-REPLACE INTO `ingredientes` (`id`, `ingre_nombre`, `ingre_cal`, `ingre_tipo`, `ingre_marca`, `ingre_vega`, `ingre_azucares`, `ingre_carbohidratos`, `ingre_colesterol`, `created_at`, `updated_at`) VALUES
-	(2, 'Lechuga', 15.00, 'Verdura', NULL, 'Si', 0.80, 2.90, 0.00, '2020-10-28 07:12:06', '2020-10-28 07:12:06'),
-	(3, 'Pepino', 16.00, 'Verdura', NULL, 'Si', 1.67, 2.00, 0.00, '2020-10-28 17:33:04', '2020-10-29 21:54:00');
-/*!40000 ALTER TABLE `ingredientes` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla come_sano.ingrediente_platillo
+CREATE TABLE IF NOT EXISTS `ingrediente_platillo` (
+  `platillo_id` bigint(20) unsigned NOT NULL,
+  `ingrediente_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`platillo_id`,`ingrediente_id`),
+  KEY `ingrediente_platillo_ingrediente_id_foreign` (`ingrediente_id`),
+  CONSTRAINT `ingrediente_platillo_ingrediente_id_foreign` FOREIGN KEY (`ingrediente_id`) REFERENCES `ingredientes` (`id`),
+  CONSTRAINT `ingrediente_platillo_platillo_id_foreign` FOREIGN KEY (`platillo_id`) REFERENCES `platillos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
@@ -95,23 +110,42 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.migrations: ~7 rows (aproximadamente)
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-REPLACE INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(1, '2020_10_24_214754_create_ingredientes_table', 1),
-	(2, '2020_10_28_000406_create_platillos_table', 1),
-	(3, '2020_10_28_001406_create_usuarios_table', 1),
-	(4, '2020_10_28_003329_create_historiales_table', 1),
-	(5, '2020_10_28_004411_create_consejos_table', 1),
-	(6, '2020_10_28_010155_create_dietas_table', 1),
-	(7, '2020_10_28_010359_create_platillos_ingredientes_table', 1);
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla come_sano.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla come_sano.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.platillos
 CREATE TABLE IF NOT EXISTS `platillos` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `plat_nombre` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `plat_cal` double(8,2) NOT NULL,
   `plat_azucares` double(8,2) NOT NULL,
   `plat_desc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -121,27 +155,44 @@ CREATE TABLE IF NOT EXISTS `platillos` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla come_sano.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `payload` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_user_id_index` (`user_id`),
+  KEY `sessions_last_activity_index` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.platillos: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `platillos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `platillos` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla come_sano.platillos_ingredientes
-CREATE TABLE IF NOT EXISTS `platillos_ingredientes` (
-  `plat_id` bigint(20) unsigned NOT NULL,
-  `ingre_id` bigint(20) unsigned NOT NULL,
+-- Volcando estructura para tabla come_sano.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `two_factor_secret` text COLLATE utf8mb4_unicode_ci,
+  `two_factor_recovery_codes` text COLLATE utf8mb4_unicode_ci,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `current_team_id` bigint(20) unsigned DEFAULT NULL,
+  `profile_photo_path` text COLLATE utf8mb4_unicode_ci,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`plat_id`,`ingre_id`),
-  KEY `platillos_ingredientes_ingre_id_foreign` (`ingre_id`),
-  CONSTRAINT `platillos_ingredientes_ingre_id_foreign` FOREIGN KEY (`ingre_id`) REFERENCES `ingredientes` (`id`),
-  CONSTRAINT `platillos_ingredientes_plat_id_foreign` FOREIGN KEY (`plat_id`) REFERENCES `platillos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.platillos_ingredientes: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `platillos_ingredientes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `platillos_ingredientes` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla come_sano.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -159,9 +210,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla come_sano.usuarios: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
